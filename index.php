8fa8d127ae43c1eb76539b457d354001a0dee8f4
June Jung <jeongjueun@gmail.com>
2013-06-03 11:37:09 -0500

    WIP: fixed typo

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ echo "Hello World<br><br>";
 
 $devNames = array("Aubrey", "June", "PamO");
 
-foreach ($devNames as $name) \
+foreach ($devNames as $name)
 {
-	echo .$name;
+	echo $name;
 }
 
 // for ($i=0; $i<=count($devNames); $i++)
