83dde2968fa9ea84762654551857242bd6a2a826
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-06-03 13:57:09 -0500

    Added doctype

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
+<!DOCTYPE HTML>
 <html>
-<head></head>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Hello World!</title> 
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    </head>
 <body>
-
 
 <?php 
 echo "Hello World<br><br>";
